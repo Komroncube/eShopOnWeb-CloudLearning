1714d1a03085bda3636c434909b1e1a3aa47bdb5
Abdujabborov Kamronbek <kamronbek_abdujabborov@epam.com>
2025-11-10 16:30:26 +0500

    Integrate Azure Function and App Insights
    Enhanced OrderCreatedHandler to notify an Azure Function about order creation via HTTP POST. Added Application Insights JavaScript snippet to _Layout.cshtml for monitoring. Updated _ViewImports.cshtml to inject JavaScriptSnippet. Included Microsoft.ApplicationInsights.AspNetCore package in Web.csproj for telemetry integration.

--- src/ApplicationCore/Entities/OrderAggregate/Handlers/OrderCreatedHandler.cs
+++ src/ApplicationCore/Entities/OrderAggregate/Handlers/OrderCreatedHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate.Events;
@@ -9,6 +11,7 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate.Handlers;
 
 public class OrderCreatedHandler(ILogger<OrderCreatedHandler> logger, IEmailSender emailSender) : INotificationHandler<OrderCreatedEvent>
 {
+    private readonly string functionUrl = "https://eshop-functions-abevcza5ahhxedf4.centralindia-01.azurewebsites.net/api/OrderItemsReserver";
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Order #{orderId} placed: ", domainEvent.Order.Id);
@@ -16,5 +19,10 @@ public class OrderCreatedHandler(ILogger<OrderCreatedHandler> logger, IEmailSend
         await emailSender.SendEmailAsync("to@test.com",
                                          "Order Created",
                                          $"Order with id {domainEvent.Order.Id} was created.");
+
+        using var httpClient = new HttpClient();
+        var response = await httpClient.PostAsJsonAsync(functionUrl, domainEvent.Order, cancellationToken);
+        logger.LogInformation("Notified external system about order #{orderId}, response status: {statusCode}, blob name: {blobname}", domainEvent.Order.Id, response.StatusCode, await response.Content.ReadAsStringAsync(cancellationToken));
+
     }
 }
